0434a58d55c17dfc5ec9f82213d25f179a25ff1a
ParamKumar-Jadvani <jadvaniparam@gmail.com>
2023-11-07 19:24:19 +0530

    07 / 11 / 2023

---
+++ Chapter-6/Conditional-Statements/Nested-Statements/6-2-2.C
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,c,d;
+	clrscr();
+	printf("Enter a :");
+	scanf("%d",&a);
+	printf("Enter b :");
+	scanf("%d",&b);
+	printf("Enter c :");
+	scanf("%d",&c);
+	printf("Enter d :");
+	scanf("%d",&d);
+	if(a == b && b == c && c ==d)
+	{
+		printf("Your All Number Have Same Value");
+	}
+	else if(a>b)
+	{
+		if(a>c)
+		{
+			if(a>d)
+			{
+				printf("a is big");
+			}
+			else
+			{
+			       printf("d is big");
+			}
+		}
+		else if (c>d)
+		{
+			printf("c is big");
+		}
+		else
+		{
+			printf("d is big ");
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			if(b>d)
+			{
+				printf("b is big");
+			}
+			else
+			{
+				printf("d is big");
+			}
+		}
+	       else if (c>d)
+	       {
+		  printf("c is big");
+	       }
+	       else
+	       {
+		printf("d is big ");
+	       }
+	}
+	getch();
+}
