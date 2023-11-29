a8ccc30a2251cd258dc98f673c1cff8f9b8677ec
ParamKumar-Jadvani <jadvaniparam@gmail.com>
2023-11-29 19:39:56 +0530

    29 / 11 / 2023

---
+++ Extra-Pattern-Practice/Without-Space/28.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j;
+	
+	for(i=5; i>=1; i--)
+	{
+		for(j=5; j>=i; j--)
+		{
+			(j%2 == 0) ? printf("| ")
+					   : printf("- ");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
