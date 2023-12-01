1a5205ea3fd41de6b26353001599d33b2d4bc39c
ParamKumar-Jadvani <jadvaniparam@gmail.com>
2023-12-01 19:13:45 +0530

    PROJECT-4

---
+++ PROJECT-4/pr-7.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j,s;
+	
+	for(i=1; i<=5; i++)
+	{
+		for(s=1; s<=5-i; s++)
+		{
+			printf("  ");
+		}
+		for(j=1; j<=(i*2)-1; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	
+	for(i=4; i>=1; i--)
+	{
+		for(s=1; s<=5-i; s++)
+		{
+			printf("  ");
+		}
+		
+		for(j=(i*2)-1; j>=1; j--)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
