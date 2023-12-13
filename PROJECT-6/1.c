8259fc146e1fc3b2c4325e5d10dfea202cca209e
ParamKumar-Jadvani <jadvaniparam@gmail.com>
2023-12-13 19:07:23 +0530

    Project-6

---
+++ PROJECT-6/1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main()
+{
+	char str[20];
+	int i,length=0;
+	
+	printf("Enter Your String :");
+	scanf("%[^\n]",&str);
+	
+	for(i=0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+	
+	char str1[length];
+	
+	for(i=0; i<length; i++)
+	{
+		str1[length-i-1] = str[i];
+	}
+	
+	for(i=0; i<length; i++)
+	{
+		if(str[i] - str1[i] == 0)
+		{
+			printf("Your string is Plaindrome !!\n");
+			break;
+		}
+		else
+		{
+			printf("Your String is Not Plaindrome !!\n");
+			break;
+		}
+	}
+	
+	
+	return 0;
+}
